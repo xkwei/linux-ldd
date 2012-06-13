32fba29d6530267efe9de2878f344c0781d77a2c
xkwei <Xia.Kewei2011@gmail.com>
2012-06-13 18:44:37 +0800

    add Makefile for helloword

--- hellowrold_dir/helloword.c
+++ hellowrold_dir/helloword.c
@@ -1,1 +1,19 @@
-linux-ldd
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+
+static int hello_init(void)
+{
+    printk(KERN_ALERT "hello world.\n");
+    return 0;
+}
+
+static void hello_exit(void)
+{
+    printk (KERN_ALERT "say goodbay!!.\n");
+}
+
+
+module_init(hello_init);
+
