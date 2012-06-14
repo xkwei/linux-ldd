f074ef72d589103afa036381f8020d870500f66f
xkwei <Xia.Kewei2011@gmail.com>
2012-06-14 09:48:57 +0800

    changes helloworld to version v0.2

--- hellowrold_dir/helloworld.c
+++ hellowrold_dir/helloworld.c
@@ -1,7 +1,14 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+#define DEVICE_AUTHOR   "xkwei  <kwxia@hbgk.net>"
+#define DEVICE_DESCRIPTIN "for helloworld test"
+
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DEVICE_AUTHOR);
+MODULE_DESCRIPTION(DEVICE_DESCRIPTIN);
+MODULE_SUPPORTED_DEVICE("helloworld");
+MODULE_VERSION("version v0.2");
 
 static int hello_init(void)
 {
